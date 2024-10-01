1a54c81916b74e4b8fe5195824b65dd15c022530
Namaztak <namaztak@gmail.com>
2024-10-01 01:42:49 -0400

    Stopped at Rate Limit

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+from circleshape import CircleShape
+import pygame
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.velocity = pygame.Vector2(0, 0)  # Initialize velocity
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (int(self.position.x), int(self.position.y)), int(self.radius), 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 import pygame
+import sys
 from constants import *
-
+from player import *
+from circleshape import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids =  pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (updatable, drawable)
+    asteroidfield = AsteroidField()
+    player = Player(
+        x = SCREEN_WIDTH / 2,
+        y = SCREEN_HEIGHT / 2
+    )
+    
+    #game loop
     while True:
+        screen.fill((0,0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        for item in updatable:
+            item.update(dt)
+        for object in asteroids:
+            if CircleShape.collision(player, object):
+                print("Game over!")
+                sys.exit()
+        for item in drawable:
+            item.draw(screen)
+        
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
+
+print("Starting asteroids!")
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
